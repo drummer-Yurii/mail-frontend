7b5c426c65d94892351bcf1f859f3e1d7c9e9117
Yurii Rybachok <drummer2009@ukr.net>
2023-06-17 16:39:08 +0300

    create project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue';
+import Modal from './components/Modal/Modal.vue';
+
+const isShow = ref(false);
+</script>
+
+<template>
+  <button @click="isShow = !isShow">Show</button>
+  <Modal :show="isShow" @close="isShow = false">This is my content modal window</Modal>
+</template>
---
+++ src/components/Modal/Modal.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { Teleport, Transition } from 'vue';
+import css from './Modal.module.css';
+
+defineEmits(["close"]);
+defineProps(['show']);
+</script>
+
+<template>
+    <Teleport to="#modal">
+        <Transition>
+            <div :class="css.modal__overlay" v-if="show" @click.self="$emit('close')">
+                <div :class="css.modal">
+                    <div :class="css.modal__header">
+                        <button @click="$emit('close')">&times;</button>
+                    </div>
+                    <div :class="css.modal__body">
+                        <slot></slot>
+                    </div>
+                </div>
+            </div>
+        </Transition>
+    </Teleport>
+</template>
+
+<style scoped>
+.v-enter-active, .v-leave-active {
+    transition: opacity 400ms ease;
+}
+.v-enter-from, .v-leave-to {
+    opacity: 0;
+}
+</style>
