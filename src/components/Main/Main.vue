39e8edbf80cdd226fb9ca0b901d5799c31598588
Yurii Rybachok <drummer2009@ukr.net>
2023-06-18 11:10:28 +0300

    create utils LettersMock

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -1,9 +1,12 @@
 <script setup>
-
+import Letter from "../Letter/Letter.vue";
 </script>
 
 <template>
     <main>
         <aside></aside>
+        <section>
+            <Letter></Letter>
+        </section>
     </main>
 </template>
---
+++ src/utils/mock.js
@@ -0,0 +1,37 @@
+export const LettersMock = [
+    {
+        title: "Hello, how are you? I'm have....",
+        isImportant: false,
+        sender: "Mira",
+        senderMail: "main@mira.com",
+        date: 1681317032706,
+    },
+    {
+        title: "Hello, how are you? I'm have....",
+        isImportant: false,
+        sender: "Mira",
+        senderMail: "main@mira.com",
+        date: 1681317032706,
+    },
+    {
+        title: "Hello, how are you? I'm have....",
+        isImportant: false,
+        sender: "Mira",
+        senderMail: "main@mira.com",
+        date: 1681317032706,
+    },
+    {
+        title: "Hello, how are you? I'm have....",
+        isImportant: false,
+        sender: "Mira",
+        senderMail: "main@mira.com",
+        date: 1681317032706,
+    },
+    {
+        title: "Hello, how are you? I'm have....",
+        isImportant: false,
+        sender: "Mira",
+        senderMail: "main@mira.com",
+        date: 1681317032706,
+    },
+]
