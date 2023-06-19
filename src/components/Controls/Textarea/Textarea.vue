661ebd5bf176be80a84ebe058eafffc7f98abc5d
Yurii Rybachok <drummer2009@ukr.net>
2023-06-19 09:22:06 +0300

    create textarea for modal window

---
+++ src/components/Controls/Textarea/Textarea.vue
@@ -0,0 +1,7 @@
+<script setup>
+import css from "./Textarea.module.css";
+</script>
+
+<template>
+    <textarea spellcheck></textarea>
+</template>
--- src/components/Editor/Editor.vue
+++ src/components/Editor/Editor.vue
@@ -1,6 +1,8 @@
 <script setup>
 import Input from '../Controls/Input/Input.vue';
 import Button from '../Controls/Button/Button.vue';
+import Textarea from '../Controls/Textarea/Textarea.vue';
+import css from "./Editor.module.css";
 
 function sendMail() {
 
@@ -8,8 +10,9 @@ function sendMail() {
 </script>
 
 <template>
-    <form @submit.prevent="sendMail">
+    <form @submit.prevent="sendMail" :class="css.editor">
         <Input styleType="form" placeholder="letter subject" />
+        <Textarea :class="css.editor__area"></Textarea>
         <Button>Send</Button>
     </form>
 </template>
